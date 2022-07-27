52ab0bdb3ad0be8443ddfdcc5c0eeb8a1da421a1
queiroz96 <viniciusqueirosz@outlook.com>
2022-07-27 20:13:45 -0300

    melhorando JS

--- script.js
+++ script.js
@@ -10,23 +10,19 @@ function calcular(event){
 
     peso = document.getElementById('peso').value;
     altura = document.getElementById('altura').value;
+    resultado = document.getElementById('resultado')
 
     imc = peso / (altura * altura);
 
     if(imc < 17){
-        resultado = document.getElementById('resultado')
         resultado.innerHTML ='</br> Seu resultado foi: ' + imc.toFixed(2) + '<br>Cuidado, você está muito abaixo do peso'
     }else if(imc > 17 && imc < 18.49){
-        resultado = document.getElementById('resultado')
         resultado.innerHTML ='</br> Seu resultado foi: ' + imc.toFixed(2) + '<br>Cuidado, você está abaixo do peso'
     }else if(imc >= 18.5 && imc <= 24.99){
-        resultado = document.getElementById('resultado')
         resultado.innerHTML ='</br> Seu resultado foi: ' + imc.toFixed(2) + '<br>Parabéns, você está em seu peso normal'
     }else if(imc > 25 &&  imc <= 29.99){
-        resultado = document.getElementById('resultado')
         resultado.innerHTML ='</br> Seu resultado foi: ' + imc.toFixed(2) + '<br>Cuidado, você está acima do peso'
     }else if(imc > 29.99){
-        resultado = document.getElementById('resultado')
         resultado.innerHTML ='</br> Seu resultado foi: ' + imc.toFixed(2) + '<br>Cuidado!! Você está com obesidade'
     }
 
